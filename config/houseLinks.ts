0e59bf1efbfa9d3bc7fa7342fcf95b1dbcab4e99
PERFECTION <akinbinuayomide1@gmail.com>
2025-11-28 06:58:50 +0100

    Update README and replace WhatsApp links with dummy links

--- config/houseLinks.ts
+++ config/houseLinks.ts
@@ -1,8 +1,7 @@
-export const HOUSE_LINKS: Record<string, string> = {
-  stark: "https://chat.whatsapp.com/CCbJxRObbPE9W4IREn6YBY?mode=hqrt2",
-  baratheon: "https://chat.whatsapp.com/DhvLw6Za0hKJLlCYkxz70b?mode=hqrt2",
-  greyjoy: "https://chat.whatsapp.com/GLApCfuhlEoEnN7WHsL2gP?mode=hqrt2",
-  lannister: "https://chat.whatsapp.com/LeIXiKaJwJGCYyzZoNEq11?mode=hqrt2",
-  targaryen: "https://chat.whatsapp.com/I7b2jBIKNKAAzuWPAGDs90?mode=hqrt2"
+  stark: "https://chat.whatsapp.com/DUMMY_LINK",
+  baratheon: "https://chat.whatsapp.com/DUMMY_LINK",
+  greyjoy: "https://chat.whatsapp.com/DUMMY_LINK",
+  lannister: "https://chat.whatsapp.com/DUMMY_LINK",
+  targaryen: "https://chat.whatsapp.com/DUMMY_LINK"
 };
 
